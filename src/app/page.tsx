18d78609d936c7778da0d3694608944ee4793816
Ricardo <ricardo_cabista@hotmail.com>
2023-10-31 09:17:43 -0300

    feat: initial adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,75 @@
-import HeroComponent from '@/components/molecules/Hero'
-import { getHeroes } from '@/utils/getheroes'
-import Image from 'next/image'
+'use client'
+import { useEffect, useState } from 'react'
 
-export default async function Home() {
-  const data = await getHeroes()
+import { getHeroes } from '@/utils/getheroes'
+
+import HeroComponent from '@/components/molecules/Hero'
+import { Modal } from '@/components/molecules/modal'
+
+export default function Home({ initialData }) {
+  const [selectedCards, setSelectedCards] = useState([])
+  const [heroes, setHeroes] = useState([])
+
+  useEffect(() => {
+    if (!initialData) {
+      const fetchData = async () => {
+        try {
+          const data = await getHeroes()
+          setHeroes(data)
+        } catch (error) {
+          console.error('Erro ao buscar heróis:', error)
+        }
+      }
+      fetchData()
+    }
+  }, [initialData])
+
+  const handleCardClick = (id: number) => {
+    if (selectedCards.length < 2) {
+      if (selectedCards.includes(id)) {
+        setSelectedCards(selectedCards.filter((cardId) => cardId !== id))
+      } else {
+        setSelectedCards([...selectedCards, id])
+      }
+    }
+  }
+
+  const isReady = Boolean(selectedCards?.length === 2 ?? 0)
+  const clearSelecteds = () => {
+    return setSelectedCards([])
+  }
+
+  const removeCurrentHero = (id: number) => {
+    const current = selectedCards.splice(id, selectedCards.length)
+    const filtered = selectedCards.filter((hero) => hero.id !== current.id)
+
+    if (filtered) {
+      setSelectedCards(filtered)
+    }
+  }
 
   return (
     <main className='flex min-h-screen flex-col items-center container  m-auto'>
+      {isReady && (
+        <Modal
+          clear={clearSelecteds}
+          allHeroes={heroes}
+          selecteds={selectedCards}
+        />
+      )}
       <h1 className='text-6xl py-10 text-left w-full'>Heroes app</h1>
       <ul className='grid md:grid-cols-4 gap-4 sm:grid-cols-2 w-full  '>
-        {data.slice(0, 42).map((data: any) => (
+        {heroes.slice(0, 40).map((data: any, index: number) => (
           <HeroComponent
             name={data.name}
             combat={data.powerstats.combat}
             image={data.images}
             id={data.id}
+            index={index}
             key={data.id}
+            selected={selectedCards?.includes(data?.id) ?? ''}
+            handleCardClick={handleCardClick}
+            removeCurrentHero={removeCurrentHero}
           />
         ))}
       </ul>
--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -1,23 +1,32 @@
 import { HeroTypes } from '@/components/atoms/types'
-import { Dumbbell } from 'lucide-react'
+import { Dumbbell, Trash2 } from 'lucide-react'
 import { FunctionComponent } from 'react'
+
+import Image from 'next/image'
 
 const HeroComponent: FunctionComponent<HeroTypes> = ({
   name,
   image,
   combat,
-  id
+  id,
+  selected,
+  index,
+  handleCardClick,
+  removeCurrentHero
 }) => {
   return (
     <li
-      className='flex flex-col justify-between bg-white rounded-lg md:w-autosm:w-full relative'
+      className={`flex flex-col justify-between bg-white rounded-lg md:w-autosm:w-full relative p-6 border border-gray-50 
+       ${selected && 'border-black'}`}
       key={id}
     >
       <div className='relative'>
-        <img
-          src={image.lg}
+        <Image
+          src={image?.lg}
           alt={`${name} image`}
-          className='object-cover rounded-lg'
+          className='object-cover w-full rounded-lg'
+          width={300}
+          height={300}
         />
         <div className='p-6 flex items-center justify-between absolute bottom-2 w-full text-white'>
           <div>
@@ -28,12 +37,31 @@ const HeroComponent: FunctionComponent<HeroTypes> = ({
             <Dumbbell />
           </div>
         </div>
+        {selected && (
+          <div className='absolute left-0 w-full h-full flex  top-0 items-center justify-center bg-black bg-opacity-60 rounded-lg'>
+            <p className='font-semibold text-xl bg-gradient-to-br from-black to-green-950 text-white rounded-lg py-2 px-4'>
+              Player {index + 1}
+            </p>
+          </div>
+        )}
       </div>
 
-      <div className='p-6'>
-        <button className='bg-gradient-to-br from-black to-red-950 w-full py-4 px-8 rounded-lg text-white'>
-          Select hero
-        </button>
+      <div className='pt-6'>
+        {selected ? (
+          <button
+            className='bg-white rounded-lg w-full py-4 px-8 text-black border flex items-center justify-center gap-6'
+            onClick={() => removeCurrentHero(index)}
+          >
+            REMOVE <Trash2 />
+          </button>
+        ) : (
+          <button
+            className='bg-gradient-to-br from-black to-green-950 w-full py-4 px-8 rounded-lg border text-white'
+            onClick={() => handleCardClick(id)}
+          >
+            SELECT HERO
+          </button>
+        )}
       </div>
     </li>
   )
